3e089778faaa3d00d916cc33897f526e2db897fc
jwon42 <jwon@student.42seoul.kr>
2021-09-06 14:29:03 +0900

    feat: add hand-writing index, enhanced for loop

--- src/com/javapractice03/ex05/Student.java
+++ src/com/javapractice03/ex05/Student.java
@@ -19,13 +19,13 @@ public class Student {
     /**
      * 학생이 수강한 과목의 ArrayList입니다.
      */
-    private ArrayList<Subject> subjectList;
+    private ArrayList<Subject> subjects;
 
     /**
      * 초기화 블록을 이용한 subjectList의 초기화
      */
     {
-        this.subjectList = new ArrayList<>();
+        this.subjects = new ArrayList<>();
     }
 
     /**
@@ -44,19 +44,41 @@ public class Student {
      * @param subjectScore 과목 점수입니다.
      */
     public void addSubject(String subjectName, int subjectScore) {
-        subjectList.add(new Subject(subjectName, subjectScore));
+        subjects.add(new Subject(subjectName, subjectScore));
     }
 
     /**
      * 학생이 수강한 모든 과목의 성적과 총점을 출력하기 위한 메서드입니다.
      */
-    public void showStudentInfo() {
+    public void showStudentInfo() { // ver1. hand-writing index
         int totalScore = 0;
-        Iterator<Subject> iter = subjectList.iterator();
-        while (iter.hasNext()) {
-            Subject temp = iter.next();
-            System.out.printf("학생 %s의 %s 과목 성적은 %d입니다.\n",this.studentName, temp.getSubjectName(), temp.getSubjectScore());
-            totalScore += temp.getSubjectScore();
+
+        for (int i = 0; i < subjects.size(); i++) {
+            Subject subject = (Subject)subjects.get(i);
+            System.out.printf("학생 %s의 %s 과목 성적은 %d입니다.\n", this.studentName, subject.getSubjectName(), subject.getSubjectScore());
+            totalScore += subject.getSubjectScore();
+        }
+        System.out.printf("학생 %s의 총점은 %d입니다.\n", this.studentName, totalScore);
+    }
+
+    public void showStudentInfo2() { // ver2. iterator
+        int totalScore = 0;
+        Iterator<Subject> iterator = null;
+
+        for (iterator = subjects.iterator(); iterator.hasNext(); ) {
+            Subject subject = (Subject)iterator.next();
+            System.out.printf("학생 %s의 %s 과목 성적은 %d입니다.\n", this.studentName, subject.getSubjectName(), subject.getSubjectScore());
+            totalScore += subject.getSubjectScore();
+        }
+        System.out.printf("학생 %s의 총점은 %d입니다.\n", this.studentName, totalScore);
+    }
+
+    public void showStudentInfo3() { // ver3. enhanced for loop
+        int totalScore = 0;
+
+        for (Subject subject : subjects) {
+            System.out.printf("학생 %s의 %s 과목 성적은 %d입니다.\n", this.studentName, subject.getSubjectName(), subject.getSubjectScore());
+            totalScore += subject.getSubjectScore();
         }
         System.out.printf("학생 %s의 총점은 %d입니다.\n", this.studentName, totalScore);
     }
