4139064a5062e347ea35a7c665822e488e8dfc90
jwon42 <jwon@student.42seoul.kr>
2021-09-29 19:57:12 +0900

    feat: add hook method

--- src/com/javapractice05/ex02/Car.java
+++ src/com/javapractice05/ex02/Car.java
@@ -3,7 +3,7 @@ package com.javapractice05.ex02;
 public abstract class Car {
     public abstract void drive();
     public abstract void stop();
-    public abstract void wiper();
+    public void wiper() {};
 
     public void startCar() {
         System.out.println("시동을 켭니다.");
